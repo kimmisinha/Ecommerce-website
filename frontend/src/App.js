ea51ca20a185170c8aa8d7ead656c60041a62591
kimmisinha <kimmi.sinha@newput.com>
2024-06-13 10:56:55 +0530

    Added sans-serif fonts to the navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Interior } from './components/Interior-Design/Interior.jsx';
+import Interior from './components/Interior-Design/Interior'
 function App() {
   return (
     <div className="App">
